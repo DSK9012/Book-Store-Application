1f57c33234e58434439152107fd3ef8e877ff4c6
srinivasaraodannana <saikiran.dannana@gmail.com>
2022-03-19 13:56:13 +0530

    introduced eslint

---
+++ client/src/components/Main.jsx
@@ -0,0 +1,9 @@
+export default function Main() {
+  return (
+    <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center', height: '100vh', width: '100%' }}>
+      <p>All set, lets start buddy...</p>
+      <br />
+      <p>HAPPY CODING...🤩</p>
+    </div>
+  );
+}
--- server.js
+++ server.js
@@ -1,20 +1,20 @@
-require("dotenv").config();
-const express=require('express');
-const cors=require('cors');
-const chalk=require('chalk');
-const morgan=require('morgan');
-const app=express();
-const connectToMongoDB=require('./DBConnections');
-const serverConfig=require('./serverConfig');
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const chalk = require('chalk');
+const morgan = require('morgan');
+const app = express();
+const connectToMongoDB = require('./DBConnections');
+const serverConfig = require('./serverConfig');
 
 // Allow Cors
 app.use(cors());
 
 // Http Requests Logger
-app.use(morgan('dev')); 
+app.use(morgan('dev'));
 
 // Intializing Express Middleware
-app.use(express.json({extended:false}));
+app.use(express.json({ extended: false }));
 
 // Routes
 app.use('/api/books', require('./serverRoutes/books/booksRouter'));
@@ -23,14 +23,14 @@ app.use('/api/books', require('./serverRoutes/books/booksRouter'));
 connectToMongoDB();
 
 // Server Connection
-const serverPort=serverConfig.serverPort;
-app.listen(serverPort, ()=>console.log(chalk.bgGreen.bold(`Book store app listening at ${serverPort}`))); 
+const serverPort = serverConfig.serverPort;
+app.listen(serverPort, () => console.log(chalk.bgGreen.bold(`Book store app listening at ${serverPort}`)));
 
 // Production Mode
-if(process.env.NODE_ENV.toLowerCase()==="production") {
+if (process.env.NODE_ENV.toLowerCase() === 'production') {
   // Serve Any Static Files
-  app.use(express.static(path.join(__dirname, "client/build")));
-  app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "client/build", "index.html"));
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
